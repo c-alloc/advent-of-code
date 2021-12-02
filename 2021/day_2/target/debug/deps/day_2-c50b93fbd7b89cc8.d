46427dfc3bb10a9dd1882c99238b4d43692bf1dd
reboottext <matheuscordeirobm@gmail.com>
2021-12-02 16:50:30 -0300

    remove files

--- 2021/day_2/target/debug/deps/day_2-c50b93fbd7b89cc8.d
+++
@@ -1,5 +0,0 @@
-/home/tera/Documents/Projects/advent-of-code/2021/day_2/target/debug/deps/day_2-c50b93fbd7b89cc8: src/main.rs
-
-/home/tera/Documents/Projects/advent-of-code/2021/day_2/target/debug/deps/day_2-c50b93fbd7b89cc8.d: src/main.rs
-
-src/main.rs:
